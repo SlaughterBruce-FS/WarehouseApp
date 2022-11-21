e303b9fa3479bd62e31e3da117d612d96e8f0c64
bruce.slaughter <BLSlaughter1@student.fullsail.edu>
2022-11-21 16:07:46 -0500

    Created the classes, started working on the user, menu, and app class

--- WaereHouseApp/App.cs
+++ WaereHouseApp/App.cs
@@ -1,11 +1,46 @@
 ﻿using System;
 namespace WaereHouseApp
 {
-	public class App
-	{
-		public App()
-		{
-		}
-	}
+    public class App
+    {
+        private Menu _menu;
+        private bool _loggedIn = false;
+        private bool _keepgoing = true;
+
+        public App()
+        {
+
+            _keepgoing = true;
+            while (_keepgoing)
+            {
+                if (!_loggedIn)
+                {
+                    Menu _menu = new();
+                    //add values to menu
+                    _menu.Init("Main Menu", new string[] { "Create User", "Login", "About", "Exit" });
+                    //display menu
+                    _menu.Display();
+                }
+                else
+                {
+                    Menu _menu = new();
+                    //add values to menu
+                    _menu.Init($"Welcome jade", new string[] { "About", "Show Items", "Add Item", "Logut", "Exit" });
+                    //display menu
+                    _menu.Display();
+                }
+
+
+                //call selcetion method and pass down the main user
+                Selection();
+            }
+        }
+
+
+        private void Selection()
+        {
+            UI.Footer("Press any Button to continue!");
+        }
+    }
 }
 
---
+++ WaereHouseApp/User.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace WaereHouseApp
+{
+    public class User
+    {
+        //set auto properties
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        //the constructor
+        public User(string name, string password)
+        {
+            //set variables
+            Name = name;
+            Password = password;
+        }
+
+        //Method to lof in user
+        public static bool Login(User user)
+        {
+            //clear console
+            Console.Clear();
+
+            UI.Header("Sign In");
+
+
+
+            //ask for username
+            Console.Write("Username: _");
+            //get username
+            string username = Console.ReadLine();
+
+            //validate password
+            Validation.ValidateString(username, "this cant be left blank", "Username: _");
+
+            //ask for password
+            Console.Write("Password: _");
+            //get password
+            string password = Console.ReadLine();
+
+            //validate password
+            Validation.ValidateString(password, "this cant be left blank", "Password: _");
+
+
+            //check if password ans user id match the default 
+
+            if (password == user.Password && username == user.Name)
+            {
+                //counsole out if true and return true
+                Console.WriteLine("\r\nUser Found");
+                return true;
+            }
+            else
+            {
+                //if it doesnt match console out error return false
+                Console.WriteLine("\r\nUser login not recognized");
+                return false;
+            }
+        }
+    }
+}
+
